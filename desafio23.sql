57abc69c41893d747a20a66ff9415519aa5d3a45
PedroMarqDev <PedroMarqDev@gmail.com>
2021-02-23 18:03:33 -0300

    Number fixes

--- desafio23.sql
+++ desafio23.sql
@@ -1,4 +1,4 @@
 SET SQL_SAFE_UPDATES = 0;
 UPDATE northwind.order_details
 SET discount = 30
-WHERE unit_price < 10.0000;
+WHERE unit_price < 10000;
--- desafio24.sql
+++ desafio24.sql
@@ -1,5 +1,5 @@
 SET SQL_SAFE_UPDATES = 0;
 UPDATE northwind.order_details
 SET discount = 30
-WHERE unit_price > 10.0000 AND
+WHERE unit_price > 10000 AND
 id BETWEEN 30 AND 40;
