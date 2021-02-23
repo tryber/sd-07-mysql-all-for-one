5da6905760296412b4562e9008059e45fb77e8f4
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-02-23 18:47:09 -0300

    desafio20.sql

--- desafio19.sql
+++ desafio19.sql
@@ -1,5 +1,2 @@
-SELECT COUNT(*) AS orders_count 
-FROM northwind.orders
-WHERE employee_id = 5 
-OR employee_id = 6 
-AND shipper_id = 2;
+SELECT COUNT(employee_id) FROM northwind.orders
+WHERE employee_id IN (5, 6) AND shipper_id = 2;
