eaef092a929a2cdb69c23c9420b12f381468627e
julioKauer <juliokauer@gmail.com>
2021-02-23 17:42:31 -0300

    Code Climate fix issues

--- desafio13.sql
+++ desafio13.sql
@@ -1,5 +1,5 @@
 SELECT supplier_id FROM northwind.purchase_orders
-	WHERE
-		supplier_id = 1
-	OR
-		supplier_id = 3;
+    WHERE
+        supplier_id = 1
+    OR
+        supplier_id = 3;
--- desafio18.sql
+++ desafio18.sql
@@ -1,2 +1,2 @@
 SELECT * FROM northwind.purchase_orders
-	WHERE supplier_id = 3 AND status_id = 2;
+    WHERE supplier_id = 3 AND status_id = 2;
