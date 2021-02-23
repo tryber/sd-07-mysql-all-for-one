315b4acf6c8a6b7b60638ddba21756d6e7c37a5d
samantabelow <samanta.below@gmail.com>
2021-02-23 17:27:09 -0300

    Ajustes

--- desafio22.sql
+++ desafio22.sql
@@ -1,4 +1,4 @@
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE northwind.order_details
-SET discount = '15';
+SET discount = 15;
--- desafio26.sql
+++ desafio26.sql
@@ -1,4 +1,4 @@
 SET SQL_SAFE_UPDATES = 0
 
 DELETE from northwind.order_details
-WHERE unit_price > '10000';
+WHERE unit_price > 10000;
