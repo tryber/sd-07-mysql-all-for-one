10899c90050a4ee5422fdbe381723d72cf9b8c3d
wberilo <wberilo@gmail.com>
2021-02-23 17:23:52 -0300

    fixes + 14-16

--- desafio14.sql
+++ desafio14.sql
@@ -1,1 +1,1 @@
-SELECT * FROM purchase_orders WHERE supplier_id BETWEEN 1 AND 3;
+SELECT * FROM northwind.purchase_orders WHERE supplier_id BETWEEN 1 AND 3;
--- desafio7.sql
+++ desafio7.sql
@@ -1,1 +1,1 @@
-SELECT * FROM (SELECT * FROM northwind.products ORDER BY id DESC LIMIT 5) sub ORDER BY id ASC;
+SELECT id FROM northwind.products ORDER BY id DESC LIMIT 5; 
