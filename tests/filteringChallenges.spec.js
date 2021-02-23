f6dd07724fc03e0bd776605ca8773b578426a163
TIAGO <tiagoesdrasvieira@gmail.com>
2021-02-23 16:28:26 -0300

    Requisito 10

--- tests/filteringChallenges.spec.js
+++ tests/filteringChallenges.spec.js
@@ -34,7 +34,7 @@ describe('Desafios sobre filtragem de dados', () => {
   });
 
   describe('Mostre todos os dados da tabela `purchase_orders` em ordem decrescente ordenados por `created_by` em que o `created_by` é maior ou igual a 3', () => {
-    it.skip('Verifica o desafio10', async () => {
+    it('Verifica o desafio10', async () => {
       const challengeQuery = readFileSync('desafio10.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult10');
 
