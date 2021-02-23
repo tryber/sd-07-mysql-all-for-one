b24c814a134e028099bc640d90d8d1dce157f952
Strongreen <strongreen@live.com>
2021-02-23 18:37:06 -0300

    Desafios 1 a 27 concluidos

---
+++ desafio12.sql
@@ -0,0 +1,3 @@
+SELECT submitted_date
+FROM northwind.purchase_orders
+WHERE date("2006-04-26");
---
+++ desafio17.sql
@@ -0,0 +1,4 @@
+SELECT ID,
+    supplier_id
+FROM northwind.purchase_orders
+WHERE supplier_id IN (1, 3, 4, 7);
