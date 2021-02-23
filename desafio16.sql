f3e6a09f7739eed77abcdd8bb1e7cb90d8d56050
marcelo ivan <sagacello@hotmail.com>
2021-02-23 16:28:45 -0300

    ate o 19

---
+++ desafio16.sql
@@ -0,0 +1,3 @@
+SELECT submitted_date FROM northwind.purchase_orders WHERE (submitted_date) 
+between '2006-01-26 00:00:00%'
+and '2006-03-31 00:00:00%';
---
+++ desafio18.sql
@@ -0,0 +1,1 @@
+SELECT * from northwind.purchase_orders where supplier_id = 3 and status_id = 2;
