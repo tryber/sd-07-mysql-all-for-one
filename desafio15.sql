f46d59190d8ab93889575fba3f7945b03aef384c
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-02-23 20:47:26 -0300

    ajustes no requisitos 15, 17 e 27

--- desafio15.sql
+++ desafio15.sql
@@ -1,1 +1,2 @@
-SELECT SUBSTRING(submitted_date,12,2) AS "submitted_hour" FROM northwind.purchase_orders;
+SELECT HOUR(submitted_date) AS "submitted_hour"
+FROM northwind.purchase_orders;
--- desafio27.sql
+++ desafio27.sql
@@ -1,1 +1,1 @@
-TRUNCATE FROM northwind.order_details;
+TRUNCATE northwind.order_details;
