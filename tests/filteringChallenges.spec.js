502c0bfb60084e9756523cff7d0c40e7e27b4ba8
TIAGO <tiagoesdrasvieira@gmail.com>
2021-02-23 17:59:23 -0300

    Requisito 19

--- tests/filteringChallenges.spec.js
+++ tests/filteringChallenges.spec.js
@@ -123,7 +123,7 @@ describe('Desafios sobre filtragem de dados', () => {
   });
 
   describe('Quantos pedidos foram feitos na tabela `orders` pelo `employee_id` igual a 5 ou 6, e que foram enviados através do método `shipper_id` igual a 2? Chame a coluna de orders_count', () => {
-    it.skip('Verifica o desafio19', async () => {
+    it('Verifica o desafio19', async () => {
       const challengeQuery = readFileSync('desafio19.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult19');
 
