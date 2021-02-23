6c8ba451de55a6874fe1c50ba8903ad6f24b60c2
vanderson-henrique <vhbenedito@gmail.com>
2021-02-23 15:38:49 -0300

    Correção codeclimate

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,2 @@
 SELECT notes FROM northwind.purchase_orders
-	WHERE notes BETWEEN 'Purchase generated based on Order #3' AND 
-    'Purchase generated based on Order #4';
+WHERE notes BETWEEN 'Purchase generated based on Order #3' AND 'Purchase generated based on Order #4';
