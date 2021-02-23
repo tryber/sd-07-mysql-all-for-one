fab3efb6a87e982971f18cb43dd41189a10326ac
Lorena Góes <goesloren@gmail.com>
2021-02-23 16:18:38 -0300

    fixing challenge 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
 # Monte uma query que exiba os dados da tabela products a partir do quarto registro até o décimo terceiro,
 # incluindo tanto um quanto o outro. Obs.: não use where ou order by.
 SELECT * from northwind.products
-LIMIT 13 OFFSET 4;
+LIMIT 13 OFFSET 3;
