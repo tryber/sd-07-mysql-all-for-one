7920db9955a5192d00b82427beb1fd9bf87a622f
Erling <erlingjunior@gmail.com>
2021-02-23 20:15:11 -0300

    ajustando requisitos 10 e 8

--- desafio10.sql
+++ desafio10.sql
@@ -1,2 +1,3 @@
-SELECT * FROM northwind.purchase_orders 
+SELECT * FROM northwind.purchase_orders
+WHERE created_by 
 ORDER BY created_by >= 3 DESC, id ASC;
--- desafio8.sql
+++ desafio8.sql
@@ -1,1 +1,1 @@
-SELECT CONCAT(5 + 6) AS "A", CONCAT("de") AS "Trybe", CONCAT(2 + 8)AS "eh" FROM northwind.products;
+SELECT CONCAT(5 + 6) AS 'A', CONCAT('de') AS 'Trybe', CONCAT(2 + 8)AS 'eh' FROM northwind.products;
