b249dfd85003a6dbb307fdef4a1450bc6ba79066
Strongreen <strongreen@live.com>
2021-02-23 20:00:58 -0300

    Correçoes nos desafios que foram rejeitados

--- desafio17.sql
+++ desafio17.sql
@@ -1,1 +1,1 @@
-SELECT ID, supplier_id FROM northwind.purchase_orders WHERE supplier_id IN (1, 3, 4, 7);
+SELECT ID, supplier_id FROM northwind.purchase_orders WHERE supplier_id  = 1 OR supplier_id = 3 OR supplier_id = 4 OR supplier_id = 7);
