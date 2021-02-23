e3a888b58af2a4a96113168992ea3e646eee465f
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-02-23 20:16:13 -0300

    desafios de 01 a 27-v2

--- desafio21.sql
+++ desafio21.sql
@@ -3,4 +3,3 @@ INSERT INTO northwind.order_details(order_id, product_id, quantity, unit_price,
     inventory_id) values 
     (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
     (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
-
