4598a3643d65d07915ae16a5916bc3c3335b6e87
w27M <marcos.1998226@gmail.com>
2021-02-24 11:35:14 -0300

    subindo arquivos 9 e 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
- SELECT notes FROM northwind.purchase_orders WHERE created_by >= 3 ORDER BY created_by DESC, id;
+SELECT notes FROM northwind.purchase_orders WHERE created_by >= 3 ORDER BY created_by DESC, id;
