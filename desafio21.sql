7a377be74ad41dde0a3c9e32c268bccfa53039e8
Lorena Góes <goesloren@gmail.com>
2021-02-24 05:52:08 -0300

    fixing challenge 21

--- desafio21.sql
+++ desafio21.sql
@@ -3,5 +3,5 @@
 # date_allocated: NULL, purchase_order_id: NULL e inventory_id: 129 (o ìd deve ser incrementado automaticamente).
 INSERT INTO northwind.order_details (order_id, product_id, quantity, unit_price, discount, status_id,
 date_allocated, purchase_order_id, inventory_id) VALUES
-(69, 80, 150000, 15.0000, 0, 2, NULL, NULL, 129),
-(69, 80, 150000, 15.0000, 0, 2, NULL, NULL, 129);
+(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129),
+(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
