97f2177184c0ed1fd7510e92d2b176b79d85141b
spawnred <bruno.spawnhell@gmail.com>
2021-02-23 22:39:44 -0300

    itens do 23 ao 27

---
+++ desafio23.sql
@@ -0,0 +1,4 @@
+update northwind.order_details
+set discount = '30'
+where unit_price < '10.0000';
+
---
+++ desafio24.sql
@@ -0,0 +1,3 @@
+update northwind.order_details
+set discount = '45'
+where id between '30' and '40'and unit_price > '10.0000';
