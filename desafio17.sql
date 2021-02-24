61fab7833e8125dc5fcfc39b7385db9beab9c2cf
Strongreen <strongreen@live.com>
2021-02-23 22:48:17 -0300

    Correçoes nos desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -1,1 +1,1 @@
-SELECT Id, supplier_id FROM northwind.purchase_orders WHERE supplier_id  = 1 OR supplier_id = 3 OR supplier_id = 4 OR supplier_id = 7;
+SELECT id, supplier_id FROM northwind.purchase_orders WHERE supplier_id  = 1 OR supplier_id = 3 OR supplier_id = 4 OR supplier_id = 7;
