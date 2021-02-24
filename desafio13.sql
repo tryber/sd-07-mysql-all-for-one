027ba95f1f0dfdd453f49409f0b6a30d73377a51
Elano Evaristo <elano3004@gmail.com>
2021-02-24 01:01:38 -0300

    Update desafio13.sql
    fix sintaxe deafio13

--- desafio13.sql
+++ desafio13.sql
@@ -1,8 +1,8 @@
 -- Mostre o supplier_id das purchase_orders em que o supplier_id seja 1 ou 3.
 
-SELECT supplier_id FROM northwind.purchase_orders
-WHERE supplier_id BETWEEN '1' AND '3';
-
 -- OUTRA FORMA DE FAZER
 -- SELECT supplier_id FROM northwind.purchase_orders
--- WHERE supplier_id IN (1,3);
+-- WHERE supplier_id BETWEEN '1' AND '3';
+
+SELECT supplier_id FROM northwind.purchase_orders
+WHERE supplier_id IN (1,3);
