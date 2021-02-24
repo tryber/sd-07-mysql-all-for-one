6018d1265596ebdbb7bb513135fdd8e72332944e
Erling <erlingjunior@gmail.com>
2021-02-23 22:00:54 -0300

    req 12, 13, 14

---
+++ desafio13.sql
@@ -0,0 +1,3 @@
+SELECT * FROM northwind.purchase_orders
+WHERE supplier_id LIKE '1'
+OR supplier_id LIKE '2';
---
+++ desafio14.sql
@@ -0,0 +1,5 @@
+SELECT * FROM northwind.purchase_orders
+WHERE supplier_id LIKE '1'
+OR supplier_id LIKE '2'
+OR supplier_id LIKE '3'
+ORDER BY supplier_id;
