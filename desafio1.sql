243e34f42ebe1ccb3b87a990fdedc5ca151d5562
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2021-02-26 08:49:54 -0300

    lint

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,7 @@
 SELECT product_name FROM northwind.products;
+
+Delete todos os dados em que a unit_price da tabela order_details seja menor que 10.0000
+Conte quantos registros existem em product_name de products
+ 22 Atualize os dados de discount da tabela order_details para 30 cuja unit_price seja menor que 10.0000
+23 Atualize os dados de discount do order_details para 15
+Delete todos os dados da tabela order_details
