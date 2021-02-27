fbf9e34d6356b6bd2a7ae7d6e68465bf1fa67795
murilopereira006 <muriloedu006@gmail.com>
2021-02-26 22:42:03 -0300

    acertando os ultimos detalhes

--- desafio15.sql
+++ desafio15.sql
@@ -1,1 +1,1 @@
-SELECT hour(submitted_date) AS submitted_date FROM northwind.purchase_orders;
+SELECT HOUR(submitted_date) AS submitted_date FROM northwind.purchase_orders;
--- desafio20.sql
+++ desafio20.sql
@@ -1,2 +1,2 @@
-INSERT INTO
+INSERT INTO northwind.order_details
 VALUES (69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL 129);
