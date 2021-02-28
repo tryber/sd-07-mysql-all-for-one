bebd2635206b0ea878314fc4dfe4a3f4fb1c6897
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-02-28 12:59:15 -0300

    segunda leva

---
+++ desafio17.sql
@@ -0,0 +1,12 @@
+SELECT
+	id, supplier_id
+FROM
+	northwind.purchase_orders
+WHERE
+	(supplier_id IS NOT NULL)
+    AND
+	(supplier_id = 1 OR
+    supplier_id = 3 OR
+    supplier_id = 5 OR
+    supplier_id = 7);
+    
---
+++ desafio7.sql
@@ -0,0 +1,7 @@
+SELECT 
+    id
+FROM
+    northwind.products
+ORDER BY id
+LIMIT 5
+OFFSET 40
