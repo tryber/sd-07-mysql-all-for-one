bb2adcb234d23da50cc3ed2c608c608bf5c5c813
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-02-28 15:32:27 -0300

    tentativa req 6 e 7

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,4 @@
 -- Exiba os dados das colunas product_name e id da tabela products de maneira que os resultados estejam em ordem alfabética dos nomes.
 USE northwind;
-SELECT * FROM products
-ORDER BY product_name, id;
+SELECT product_name, id FROM products
+ORDER BY product_name;
