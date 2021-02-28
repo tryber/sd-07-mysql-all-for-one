accc19366b6e9e796b061c8bedc0fe8b93f789f7
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-02-28 19:13:31 -0300

    req 24

---
+++ desafio24.sql
@@ -0,0 +1,4 @@
+-- Atualize os dados de discount da tabela order_details para 45 cuja unit_price seja maior que 10.0000 e o id seja um número entre 30 a 40.
+UPDATE northwind.order_details
+SET discount = 45
+WHERE unit_price > 10.0000 AND id IN(30,40);
