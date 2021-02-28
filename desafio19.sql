4a74a21b11a244e3bee31f530e20c0834779822b
Marcio <marcio@unknown6432a883dae2.localdomain>
2021-02-28 19:08:48 -0300

    req 19 e 24

--- desafio19.sql
+++ desafio19.sql
@@ -1,5 +1,5 @@
 SELECT
-*
+COUNT(*) as orders_count
 FROM
 northwind.orders
 WHERE
--- desafio24.sql
+++ desafio24.sql
@@ -3,6 +3,6 @@ northwind.order_details
 SET
 discount = 45
 WHERE
-unit_price < 10.0000
+unit_price > 10.0000
 AND
 (id BETWEEN 30 and 40);
