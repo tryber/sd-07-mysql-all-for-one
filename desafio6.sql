5717baeeb037e9f4ed95ebf18f162dc6e88f1dde
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2021-02-28 15:36:57 -0300

    Retirada do USE

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,3 @@
 -- Exiba os dados das colunas product_name e id da tabela products de maneira que os resultados estejam em ordem alfabética dos nomes.
-USE northwind;
-SELECT product_name, id FROM products
+SELECT product_name, id FROM northwind.products
 ORDER BY product_name;
