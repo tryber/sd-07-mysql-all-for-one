6a6a4685a87888f937f369a9dcfd568fdc0739a2
Lucas <lafdesouza2002@outlook.com>
2021-03-02 12:20:54 -0300

    Tentando corrigir alguns erros

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(case employee_id IN (5, 6) AND shipper_id = 2) AS orders_count FROM northwind.orders;
+SELECT COUNT(SELECT * FROM northwind.orders WHERE employee_id IN (5, 6) AND shipper_id = 2) AS orders_count;
