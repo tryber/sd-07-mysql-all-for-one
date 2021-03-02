b0e6e58cf5dba1bfa58d51dc5013b0c6d243c162
Thays Costa <thayscosta3@gmail.com>
2021-03-01 21:19:53 -0300

    Corrigindo alguns requisitos

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,1 @@
-SELECT created_by, id FROM northwind.purchase_orders WHERE created_by >= 3 ORDER BY created_by DESC, id ASC;
+SELECT * FROM northwind.purchase_orders WHERE created_by >= 3 ORDER BY created_by DESC, id ASC;
