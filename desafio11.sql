5de967f3c8bc3afed3da671bb1fc73b10c47d9d4
Erling <erlingjunior@gmail.com>
2021-03-08 11:52:16 -0300

    ajustando erros de sintaxe 11, 14, 16

--- desafio11.sql
+++ desafio11.sql
@@ -1,3 +1,2 @@
-SELECT * FROM northwind.purchase_orders
-WHERE substr(notes, -2) >= 30 AND substr(notes, -2) <= 39
-ORDER BY notes;
+SELECT notes FROM northwind.purchase_orders
+WHERE notes LIKE '%3_';
