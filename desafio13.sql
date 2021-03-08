6f39bf8d9afe6b91e410e6b8aeaeb1bf7042a02a
Erling <erlingjunior@gmail.com>
2021-03-08 11:17:49 -0300

    13, 22

--- desafio13.sql
+++ desafio13.sql
@@ -1,6 +1,2 @@
-/* SELECT * FROM northwind.purchase_orders
-WHERE supplier_id ='1'
-OR supplier_id = '3'; */
-
-SELECT * FROM northwind.purchase_orders
+SELECT supplier_id FROM northwind.purchase_orders
 WHERE supplier_id IN (1, 3);
